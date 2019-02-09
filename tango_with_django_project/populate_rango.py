fbf707cec93e1ee47c4257e11709e4394ecb2d84
JunpengLu <44099206+LukeJayden@users.noreply.github.com>
2019-02-09 15:31:44 +0000

    Finish the exercise of Chapt5
    Find out that I have to finish the exercise to continue

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -1,5 +1,4 @@
 import os
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings') 
 
 import django 
@@ -7,55 +6,67 @@ django.setup()
 from rango.models import Category, Page
 
 def populate(): 
- # First, we will create lists of dictionaries containing the pages 
- # we want to add into each category. 
- # Then we will create a dictionary of dictionaries for our categories. 
- # This might seem a little bit confusing, but it allows us to iterate
- # through each data structure, and add the data to our models. 
-	python_pages = [
-		{"title": "Official Python Tutorial", 
-		"url":"http://docs.python.org/2/tutorial/"}, 
-		{"title":"How to Think like a Computer Scientist",
-		"url":"http://www.greenteapress.com/thinkpython/"},
-		{"title":"Learn Python in 10 Minutes",
-		"url":"http://www.korokithakis.net/tutorials/python/"} ]
-		
-	django_pages = [ 
-		{"title":"Official Django Tutorial",
-		"url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
-		{"title":"Django Rocks",
-		"url":"http://www.djangorocks.com/"},
-		{"title":"How to Tango with Django",
-		"url":"http://www.tangowithdjango.com/"} ]
-		
-	other_pages = [
-		{"title":"Bottle",
-		"url":"http://bottlepy.org/docs/dev/"},
-		{"title":"Flask",
-		"url":"http://flask.pocoo.org"} ]
-		
-	cats = {"Python": {"pages": python_pages},
-		"Django": {"pages": django_pages},
-		"Other Frameworks": {"pages": other_pages} } 
+	python_cat = add_cat('Python', 128, 64)
 	
-	for cat, cat_data in cats.items(): 
-		c = add_cat(cat)
-		for p in cat_data["pages"]:
-			add_page(c, p["title"], p["url"])
+	add_page(cat=python_cat,
+            title="Learn Python in 10 Minutes",
+            url="https://www.stavros.io/tutorials/python/",
+			views=128)
+
+	add_page(cat=python_cat,
+			title="How to Think like a Computer Scientist",
+			url="http://openbookproject.net/thinkcs/python/english3e/",
+			views=128)
+
+	add_page(cat=python_cat,
+            title="Official Python Tutorial",
+            url="https://docs.python.org/3/tutorial/index.html",
+			views=128)
+
+	django_cat = add_cat('Django', 64, 32)
+
+	add_page(cat=django_cat,
+            title='How to Tango With Django',
+            url="https://www.tangowithdjango.com/",
+			views=64)
+
+	add_page(cat=django_cat,
+            title="Django Books",
+            url="http://www.djangobooks.com/",
+			views=64)
+			
+	add_page(cat=django_cat,
+            title="Offical Django Tutorial",
+            url="https://docs.djangoproject.com/en/2.1/intro/tutorial01/",
+			views=64)
+
+	otherframeworks_cat = add_cat('Other Frameworks', 32, 16)
+
+	add_page(cat=otherframeworks_cat,
+            title='Flask',
+            url="http://flask.pocoo.org/",
+			views=32)
+
+	add_page(cat=otherframeworks_cat,
+            title="Bottle",
+            url="https://bottlepy.org/docs/dev/",
+			views=32)
 	
 	for c in Category.objects.all(): 
 		for p in Page.objects.filter(category=c): 
 			print("- {0} - {1}".format(str(c), str(p))) 
 		 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
 	p = Page.objects.get_or_create(category=cat, title=title)[0] 
-	p.url=url	 
-	p.views=views
+	p.url = url	 
+	p.views = views
 	p.save()
 	return p
 	
-def add_cat(name):
-	c = Category.objects.get_or_create(name=name)[0]
+def add_cat(cat, views, likes):
+	c = Category.objects.get_or_create(name=cat)[0]
+	c.views = views
+	c.likes = likes
 	c.save()
 	return c
 
